81e3105368ded87af0ee926fd411b12792931866
GongDolHoon <qkrtmdgns23@gmail.com>
2023-04-26 18:44:35 +0900

    2309: 일곱 난쟁이 - 3

--- 2309/main.cpp
+++ 2309/main.cpp
@@ -1,6 +1,7 @@
 #include <algorithm>
 #include <cstdint>
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -8,35 +9,53 @@ uint16_t inputs[9];
 
 int main()
 {
-	// 입력을 처리함
-	// -1. 아홉 개의 줄에 걸쳐 난쟁이의 키를 입력으로 처리
+	// 난쟁이 9명에 대한 입력을 처리함
 	for (uint16_t i = 0; i < 9; ++i)
 	{
-		cin >> inputs[i]; 
+		cin >> inputs[i];
 	}
 
-	// 난쟁이의 키를 정렬해야 함
-	sort(inputs + 0, inputs + 9);
-
-	// 아홉 난쟁이 중 일곱 명을 선택하여 합이 100이 되는지 확인
-	uint16_t sum;
-	do {
-		sum = 0;
-		for (uint16_t i = 0; i < 7; ++i)
-		{
-			sum += inputs[i];
-		}
-
-		if (sum == 100)
-		{
-			break;
-		}
-	} while (next_permutation(inputs + 0, inputs + 9));
-	// 출력을 처리함 
-	// -1. 키의 합이 100이 되는 난쟁이를 오름차순으로 출력
-	for (uint16_t i = 0; i < 7; ++i) 
+	// 1. 난쟁이 9명 키의 합을 구함
+	uint16_t sum = 0;
+	for (uint16_t height : inputs)
 	{
-		cout << inputs[i] << '\n';
+		sum += height;
+	}
+
+	// 2. 9명 중 2명을 뽑음
+	pair<uint16_t, uint16_t> pair;
+
+	for (uint16_t i = 0; i < 9; ++i)
+	{
+		for (uint16_t j = 0; j < i; ++j)
+		{
+			// 3. 1번에서 구한 결과에 2번을 합한 결과를 뺌
+			if (sum - inputs[i] - inputs[j] == 100)
+			{
+				// 4. 난쟁이 7명의 키의 합이 100이 될 시, 선택한 두 명을 기억
+				pair = {i, j};
+				goto escape;
+			}
+		}
+	}
+escape:
+	// 5. 4번에서 기억한 난쟁이를 제외한 7명의 난쟁이를 선정
+	vector<uint16_t> v;
+	v.reserve(7);
+	for (uint16_t i = 0; i < 9; ++i)
+	{
+		if (i == pair.first || i == pair.second)
+		{
+			continue;
+		}
+		v.push_back(inputs[i]);
+	}
+
+	// 결과를 오름차순으로 일곱 줄로 출력
+	sort(v.begin(), v.end());
+	for (auto i : v)
+	{
+		cout << i << '\n';
 	}
 
 	return 0;
