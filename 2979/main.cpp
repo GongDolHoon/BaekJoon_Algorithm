fb2b8d2bcf31fccb49651f699388893799fa81fe
GongDolHoon <qkrtmdgns23@gmail.com>
2023-04-27 14:14:39 +0900

    2979: 트럭 주차 - 3

--- 2979/main.cpp
+++ 2979/main.cpp
@@ -1,45 +1,43 @@
 #include <cstdint>
 #include <iostream>
-#include <unordered_map>
 
 using namespace std;
 
-uint16_t a, b, c;
-pair<uint16_t, uint16_t> inputs[3];
+uint16_t A, B, C, a, b;
+uint16_t input[104] = {0, };
 uint16_t output = 0;
-unordered_map<uint16_t, uint16_t> map;
 
 int main()
 {
-	// 입력 - A, B, C (1 <= C <= B <= A <= 100)
-	cin >> a >> b >> c; 
-	// 세 줄에 두 개의 정수 입력 (1이상 100이하)
-	for (uint16_t ui = 0; ui < 3; ++ui)
+	cin >> A >> B >> C;
+	for (uint16_t i = 0; i < 3; ++i)
 	{
-		cin >> inputs[ui].first >> inputs[ui].second;
-		for (uint16_t uj = inputs[ui].first; uj < inputs[ui].second; uj++)
+		cin >> a >> b;
+		for (uint16_t j = a; j < b; ++j)
 		{
-			map.insert({uj, map[uj]++});
+			input[j] += 1;
 		}
 	}
 
-	for (auto value : map)
+	for (uint16_t i = 1; i < 100; ++i)
 	{
-		if (value.second == 1)
+		if (input[i])
 		{
-			output += a;
-		}
-		else if (value.second == 2)
-		{
-			output += b * 2;
-		}
-		else
-		{
-			output += c * 3;
+			if (input[i] == 1)
+			{
+				output += A;
+			}
+			else if (input[i] == 2)
+			{
+				output += B * 2;
+			}
+			else
+			{
+				output += C * 3;
+			}
 		}
 	}
 
-	// 출력 - 지불해야 하는 주차 요금
 	cout << output << '\n';
 
 	return 0;
